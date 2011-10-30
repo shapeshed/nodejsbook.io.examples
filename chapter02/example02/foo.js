aaa6414c507bc5c48c6002ca774611c68c730729
George Ornbo <george@shapeshed.com>
2011-10-30 13:44:36 +0000

    adding package.json example

---
+++ chapter02/example02/foo.js
@@ -0,0 +1,14 @@
+/*
+* This tiny program needs the underscore.js module to run
+* In this example we use a package.json to specify that we need the 
+* underscore library for this project
+*
+* Before running foo.js you can install underscore by running
+* `npm install`
+* This reads the package.json file and installs any specified dependencies
+*/
+
+var _ = require('underscore')
+_.each([1, 2, 3], function(num){ 
+	console.log("underscore.js says " + num)
+});
