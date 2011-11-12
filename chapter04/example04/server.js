65f1b744f436154df6a181303a2794f292c49ba2
George Ornbo <george@shapeshed.com>
2011-11-12 15:41:51 +0000

    trying to get js formatting to 2 spaces in vim

--- chapter04/example04/server.js
+++ chapter04/example04/server.js
@@ -5,34 +5,34 @@
 * pathname we serve a 404 response
 */
 var http = require('http')
-	, url = require('url');
+  , url = require('url');
 
 http.createServer(function (req, res) {
-	var pathname = url.parse(req.url).pathname;
-	switch(pathname) {
-		case '/':
-			res.writeHead(200, {
-				'Content-Type': 'text/plain'
-			});
-			res.end('Home Page\n')
-		break;
-		case '/about':
-			res.writeHead(200, {
-				'Content-Type': 'text/plain'
-			});
-			res.end('About Us\n')
-		break;
-		case '/redirect':
-			res.writeHead(301, {
-				'Location': '/'
-			});
-			res.end();
-		break;
-		default:
-			res.writeHead(404, {
-				'Content-Type': 'text/plain'
-			});
-			res.end('Page not found\n')
-	}
+  var pathname = url.parse(req.url).pathname;
+  switch(pathname) {
+    case '/':
+      res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.end('Home Page\n')
+    break;
+    case '/about':
+      res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.end('About Us\n')
+    break;
+    case '/redirect':
+      res.writeHead(301, {
+      'Location': '/'
+    });
+    res.end();
+    break;
+    default:
+      res.writeHead(404, {
+      'Content-Type': 'text/plain'
+    });
+    res.end('Page not found\n')
+  }
 }).listen(3000, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3000/');
