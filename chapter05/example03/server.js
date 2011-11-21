00e675e345434ed547c5f3683a3b3e4006a410d2
George Ornbo <george@shapeshed.com>
2011-11-21 11:20:41 +0000

    fix 2 space indentation

--- chapter05/example03/server.js
+++ chapter05/example03/server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(301, {
-	'Location': 'http://www.homestarrunner.com/sbsite/'
-  });
-	res.end();
+	res.writeHead(301, {
+		'Location': 'http://www.homestarrunner.com/sbsite/'
+	});
+		res.end();
 }).listen(3000, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3000/');
