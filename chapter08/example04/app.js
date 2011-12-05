722336832dc20cb0a4576d495a2d64eaceec1ffe
George Ornbo <george@shapeshed.com>
2011-12-05 11:22:28 +0000

    adding example on connecting to mongo

--- chapter08/example04/app.js
+++ chapter08/example04/app.js
@@ -7,22 +7,13 @@ var express = require('express')
   , routes = require('./routes')
   , mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/todo');
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
-
-function validatePresenceOf(value) {
-  return value && value.length;
-}
-
-var Task = new Schema({
-  task : { type: String, validate: [validatePresenceOf, 'a task is required'] },
-  created_at : Date,
-  updated_at : Date
+mongoose.connect('mongodb://localhost/todo_development', function(err) {
+  if (!err) {
+  console.log('connected to MongoDB');
+  } else {
+    throw err;
+  }
 });
-var Task = mongoose.model('Task', Task);
-
-
 
 var app = module.exports = express.createServer();
 
@@ -33,10 +24,8 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: "keyboard cat" }));
   app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
@@ -50,86 +39,6 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
-app.get('/tasks', function(req, res){
-  Task.find({}, function (err, docs) {
-    res.render('tasks/index.jade', { 
-        title: 'My Site',
-        docs: docs,
-        flash: req.flash()
-      });
-  });
-});
-
-// Create
-app.get('/tasks/new', function(req, res){
-  res.render('tasks/new.jade', { 
-    title: 'My Site',
-    flash: req.flash()
-  });
-});
-
-app.post('/tasks', function(req, res){
-  var task = new Task(req.body.task);
-  task.created_at = new Date();
-  task.save(function (err) {
-    if (!err) {
-      req.flash('info', 'Task created');
-      res.redirect('/tasks');
-    }
-    else {
-      req.flash('warning', err);
-      res.redirect('/tasks/new');
-    }
-  });
-});
-
-// Read
-app.get('/tasks/:id', function(req, res){
-  Task.findById(req.params.id, function (err, doc){
-    res.render('tasks/show.jade', { 
-      title: 'My Site', 
-      task: doc 
-    });
-  });
-});
-
-// Update
-app.get('/tasks/:id/edit', function(req, res){
-  Task.findById(req.params.id, function (err, doc){
-    res.render('tasks/edit.jade', { 
-      title: 'My Site', 
-      task: doc 
-    });
-  });
-});
-
-app.put('/tasks/:id', function(req, res){
-  Task.findById(req.params.id, function (err, doc){
-    doc.updated_at = new Date();
-    doc.task = req.body.task.task;
-    doc.save(function(err) {
-      if (!err){
-        req.flash('info', 'Task udpated');
-        res.redirect('/tasks');
-      }
-      else {
-        // error handling
-      }
-    });
-  });
-});
-
-// Delete
-app.del('/tasks/:id', function(req, res){
-  Task.findOne({ _id: req.params.id }, function(err, doc) {
-    if (!doc) return next(new NotFound('Document not found'));
-    doc.remove(function() {
-      req.flash('info', 'Task deleted');
-      res.redirect('/tasks');
-    });
-  });
-});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
