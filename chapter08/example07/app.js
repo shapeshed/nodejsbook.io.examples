bd7afe2ee2668c976da2064cc03faeebe819347f
George Ornbo <george@shapeshed.com>
2011-12-05 14:55:33 +0000

    adding new example

---
+++ chapter08/example07/app.js
@@ -0,0 +1,73 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/todo_development')
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var Task = new Schema({
+  task: String
+});
+
+var Task = mongoose.model('Task', Task);
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.get('/tasks', function(req, res){
+  Task.find({}, function (err, docs) {
+    res.render('tasks/index', { 
+      title: 'Todos index view',
+      docs: docs
+    });
+  });
+});
+
+app.get('/tasks/new', function(req, res){
+  res.render('tasks/new.jade', { 
+    title: 'New Task'
+  });
+});
+
+app.post('/tasks', function(req, res){
+  var task = new Task(req.body.task);
+  task.save(function (err) {
+    if (!err) {
+      res.redirect('/tasks');
+    }
+    else {
+      res.redirect('/tasks/new');
+    }
+  });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
