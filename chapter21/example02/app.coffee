51c1a91481677891ab961587891750f5531b2203
George Ornbo <george@shapeshed.com>
2011-12-20 12:25:06 +0000

    adding pre-compile example

---
+++ chapter21/example02/app.coffee
@@ -0,0 +1,6 @@
+http = require 'http'
+http.createServer (req, res) ->
+  res.writeHead 200, 'Content-Type': 'text/plain'
+  res.end 'Hello, World!\n'
+.listen 3000, '127.0.0.1'
+console.log 'Server running at http://127.0.0.1:3000/'
---
+++ chapter21/example02/app.js
@@ -0,0 +1,15 @@
+(function() {
+  var http;
+
+  http = require('http');
+
+  http.createServer(function(req, res) {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    return res.end('Hello, World!\n');
+  }).listen(3000, '127.0.0.1');
+
+  console.log('Server running at http://127.0.0.1:3000/');
+
+}).call(this);
