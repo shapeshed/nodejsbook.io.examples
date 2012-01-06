8615d07315d2b65c6c7bb33920913822f95acf6f
George Ornbo <george@shapeshed.com>
2012-01-06 11:17:27 +0000

    adding example05

--- chapter15/example04/app.js
+++ chapter15/example04/app.js
@@ -1,10 +1,11 @@
 var http = require('http')
-  , data = "";
+, data = ""
+, json = "";
 
 var options = {
-  host: '127.0.0.1',
-  port: 3000,
-  path: '/'
+  host: 'search.twitter.com',
+  port: 80,
+  path: '/search.json?q=#node.js'
 };
 
 var request = http.get(options, function(res){
@@ -13,11 +14,11 @@ var request = http.get(options, function(res){
   });
   res.on('end', function(){
     json = JSON.parse(data);
-    console.log('Name: ' + json.name);
-    console.log('Occupation: ' + json.surname);
+    for (var i=0; i<json.results.length; i++) {
+      console.log(json.results[i].text)
+    }
   });
   res.on('error', function(e){
     console.log("There was an error: " + e.message);
   });
 })
-
