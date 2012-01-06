bc12243fcf32afe06a6fc3c62a4af21b0f8877ca
George Ornbo <george@shapeshed.com>
2012-01-06 14:52:01 +0000

    example 07

---
+++ chapter15/example07/test/tasks.js
@@ -0,0 +1,24 @@
+var http = require('http')
+  , assert = require('assert')
+  , app = require('../app.js');
+
+describe('api v1', function(){
+  describe('GET /api/v1/tasks', function(){
+    it('should return a 200 response', function(done){
+      http.get({ path: '/api/v1/tasks', port: 3001 }, function(res){
+        assert.equal(res.statusCode, 
+           200,
+           'Expected: 200 Actual: ' + res.statusCode );
+        done();
+      })
+    })
+    it('should return JSON', function(done){
+      http.get({ path: '/api/v1/tasks', port: 3001 }, function(res){
+        assert.equal(res.headers["content-type"], 
+           "application/json; charset=utf-8", 
+           'Expected: application/json; charset=utf-8 Actual: ' + res.headers["content-type"]);
+        done();
+      })
+    })
+  })
+})
