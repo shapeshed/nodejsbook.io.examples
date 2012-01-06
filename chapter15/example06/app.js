9513137ab245fea6b75d91146990d1562f0efdee
George Ornbo <george@shapeshed.com>
2012-01-06 13:38:30 +0000

    add example06

---
+++ chapter15/example06/app.js
@@ -0,0 +1,22 @@
+var express = require('express')
+
+var app = module.exports = express.createServer();
+
+var rebels = [
+  { name: 'Han Solo' }
+  , { name: 'Luke Skywalker' }
+  , { name: 'C-3PO' }
+  , { name: 'R2-D2' }
+  , { name: 'Chewbacca' }
+  , { name: 'Princess Leia' }
+];
+
+app.get('/', function(req, res, next){
+  res.send(rebels);
+  res.end();
+});
+
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
