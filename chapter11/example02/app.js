0b4a44d6109a64404257550f88526f399d45d48f
George Ornbo <george@shapeshed.com>
2012-01-08 14:23:49 +0000

    more chapter11 examples

---
+++ chapter11/example02/app.js
@@ -0,0 +1,39 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+
+var port = process.env.PORT || 3000;
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/about', routes.about);
+app.get('/contact', routes.contact);
+
+app.listen(port);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
