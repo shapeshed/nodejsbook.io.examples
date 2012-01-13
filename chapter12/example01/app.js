bd0bb923693c98f9c1489029b64b259f97edd9ca
George Ornbo <george@shapeshed.com>
2012-01-13 16:27:12 +0000

    chapter 12 example01

---
+++ chapter12/example01/app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+
+var app = module.exports = express.createServer();
+var io = require('socket.io').listen(app);
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('ping', function (data) {
+    console.log('Received PING request. Sending response');
+    socket.emit('pong', { text: 'PONG' });
+  });
+});
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
