4be988d14b259619eda23c32d24227113def9df7
George Ornbo <george@shapeshed.com>
2012-02-06 13:54:36 +0000

    example03 cleanup

--- chapter22/example03/lib/ohai.js
+++
@@ -1,4 +0,0 @@
-exports.hello = function() {
-  var message = "Hello from the ohai module";
-  return message;
-};
---
+++ chapter22/example03/lib/ohaithere.js
@@ -0,0 +1,4 @@
+exports.hello = function() {
+  var message = "Hello from the ohaithere module";
+  return message;
+};
