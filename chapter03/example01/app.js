892e3c5ebe84d547a93996eee0f3d95d60fa9981
George Ornbo <george@shapeshed.com>
2012-02-15 15:47:57 +0000

    final examples for chapter03

--- chapter03/example01/app.js
+++ chapter03/example01/app.js
@@ -1,5 +1,15 @@
-setTimeout(function(){
-  console.log('World') 
-}, 2000);
+var http = require('http')
+  , urls = ['shapeshed.com', 'www.bbc.co.uk', 'edition.cnn.com']
 
-console.log('Hello');
+function fetchPage(url) {
+  var start = new Date();
+  http.get({ host: url }, function(res) {
+    console.log("Got response from: " + url);
+    console.log('Request took:', new Date() - start, 'ms');
+  });
+}
+
+for(var i = 0; i < urls.length; i++) {
+  fetchPage(urls[i]);
+}
+
