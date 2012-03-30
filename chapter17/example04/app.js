c9f9d61ebebd02651be899453fd4ed3ddc12e80f
George Ornbo <george@shapeshed.com>
2012-03-30 16:14:10 +0100

    adding chapter17 examples

---
+++ chapter17/example04/app.js
@@ -0,0 +1,23 @@
+var cluster = require('cluster')
+  , http = require('http')
+  , cpus = 2;
+
+if (cluster.isMaster) {
+  console.log('Master process started with PID:', process.pid);
+
+  for (var i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('death', function(worker) {
+    console.log('worker ' + worker.pid + ' died');
+    cluster.fork();
+  });
+
+} else {
+  console.log('Worker process started with PID:', process.pid);
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+  }).listen(3000);
+}
