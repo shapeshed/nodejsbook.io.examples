5b0950a4cf24e603c8fed3ee97fe2d73c27af187
George Ornbo <george@shapeshed.com>
2012-05-14 11:24:15 +0100

    update example following TE

--- chapter12/example03/app.js
+++ chapter12/example03/app.js
@@ -1,5 +1,6 @@
-var http = require('http')
-  , fs = require('fs');
+var http = require('http'),
+  fs = require('fs');
+
 var server = http.createServer(function (req, res) {
     fs.readFile('./index.html', function(error, data) {
       res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -12,6 +13,6 @@ var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
   socket.on('message', function (data) {
-    socket.broadcast.emit('broadcast', data);
+    socket.broadcast.emit('push message', data);
   });
 });
