7005876b7ada1fa41a76785dd0c557276de3efd8
George Ornbo <george@shapeshed.com>
2013-08-09 16:49:22 +0100

    [fix] update for Express 3.0.x

--- hour06/example05/app.js
+++ hour06/example05/app.js
@@ -5,31 +5,31 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
 
-var app = module.exports = express.createServer();
+var app = express();
 
-// Configuration
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
 app.get('/', routes.index);
+app.get('/users', user.list);
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
