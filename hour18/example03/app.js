022ccb1f0b86108a387787dab8acca7bbd50aa31
George Ornbo <george@shapeshed.com>
2013-08-09 17:15:42 +0100

    [fix] update for Twitter 1.1 API

--- hour18/example03/app.js
+++ hour18/example03/app.js
@@ -1,13 +1,15 @@
-var https = require('https');
-var username = 'YOUR_TWITTER_USERNAME';
-var password = 'YOUR_TWITTER_PASSWORD';
-var json;
+var https = require('https'),
+  json;
 
 var options = {
   host: 'stream.twitter.com',
-  auth: username + ":" + password,
-  path: '/1/statuses/filter.json?track=chocolate',
-  method: 'POST'
+  path: '/1.1/statuses/filter.json?track=chocolate',
+  method: 'POST',
+  headers: {
+    // Copy the OAuth Authorization here
+    // Sign in at https://dev.twitter.com/
+    authorization: 'OAuth oauth_consumer_key="[REPLACE]", oauth_nonce="[REPLACE]", oauth_signature="[REPLACE]", oauth_signature_method="HMAC-SHA1", oauth_timestamp="[REPLACE]", oauth_token="[REPLACE]", oauth_version="1.0"'
+  }
 };
 
 var req = https.request(options, function(res) {
