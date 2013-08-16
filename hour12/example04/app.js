e525b41959b1a3a2cf1605da1eb98a7ca88618f1
George Ornbo <george@shapeshed.com>
2013-08-16 15:21:42 +0100

    [fix] variable declaration cleanup

--- hour12/example04/app.js
+++ hour12/example04/app.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var fs = require('fs');
+var http = require('http'),
+  fs = require('fs');
 
 var server = http.createServer(function (req, res) {
   fs.readFile('./index.html', function(error, data) {
