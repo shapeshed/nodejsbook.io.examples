27e5a5a6e6eec179f6277bdf3c381b562438dfda
George Ornbo <george@shapeshed.com>
2013-08-16 14:38:37 +0100

    [fix] upgrade examples for express 3.x

--- hour07/example04/app.js
+++ hour07/example04/app.js
@@ -5,29 +5,26 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
 
-var app = module.exports = express.createServer();
+var app = express();
 
-// Configuration
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
 app.get('/', routes.index);
 
@@ -35,5 +32,6 @@ app.get('/users/:id', function(req, res){
   res.send('show content for user id ' + req.params.id);
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
--- hour07/example05/app.js
+++ hour07/example05/app.js
@@ -1,33 +1,29 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
   , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
 
-var app = module.exports = express.createServer();
+var app = express();
 
-// Configuration
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
 app.get('/', function(req, res){
   var user = { 
@@ -36,8 +32,9 @@ app.get('/', function(req, res){
     address: 'The White House',
     facebook_friends: '10000000000000'
   };
-  res.render('index.jade', { title: 'User', user: user  });
+  res.render('index', { title: 'User', user: user })
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
