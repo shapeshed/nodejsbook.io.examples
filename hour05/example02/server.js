3b805ec4585c8d4277f28fbed5b4fd14aa19d6be
George Ornbo <george@shapeshed.com>
2013-08-16 14:37:49 +0100

    [fix] cleanup

--- hour05/example02/server.js
+++ hour05/example02/server.js
@@ -1,8 +1,9 @@
 var http = require('http');
+
 http.createServer(function (req, res) {
   console.log(req);
   res.writeHead(200, {
-	'Content-Type': 'text/plain'
+    'Content-Type': 'text/plain'
   });
   res.end('Hello World\n');
 }).listen(3000, "127.0.0.1");
--- hour05/example03/server.js
+++ hour05/example03/server.js
@@ -1,8 +1,9 @@
 var http = require('http');
+
 http.createServer(function (req, res) {
   res.writeHead(301, {
     'Location': 'http://www.homestarrunner.com/sbsite/'
   });
-    res.end();
+  res.end();
 }).listen(3000, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3000/');
