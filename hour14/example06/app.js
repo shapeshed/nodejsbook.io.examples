591824cdf6b8bf8fb8d8209bbffd05314bc3d803
George Ornbo <george@shapeshed.com>
2014-01-21 13:13:21 +0000

    [fix] express 3

--- hour14/example06/app.js
+++ hour14/example06/app.js
@@ -1,11 +1,14 @@
-var app = require('express').createServer(),
-    twitter = require('ntwitter'),
-    io = require('socket.io').listen(app),
-    love = 0,
-    hate = 0,
-    total = 0;
+var express = require('express'),
+  twitter = require('ntwitter'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  love = 0,
+  hate = 0,
+  total = 0;
 
-app.listen(3000);
+
+app.set('port', process.env.PORT || 3000);
 
 var twit = new twitter({
   consumer_key: 'YOUR_CONSUMER_KEY',
@@ -52,3 +55,6 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
